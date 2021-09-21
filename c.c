28e543261ec622e9e200a649c9bbc5ebd490619d
Ajieu <2545889004@qq.com>
2021-09-22 00:02:42 +0800

    Create c.c

---
+++ c.c
@@ -0,0 +1,60 @@
+//#include<stdio.h>
+//int main()
+//{
+//	float a, b;
+//	scanf_s("%f  %f",&a,&b);
+//	float max = a;
+//	if (b > max)
+//		printf("max=%f\n", b);
+//	else
+//		printf("max=%f\n", a);
+//	return 0;
+//
+//}
+//#include<stdio.h>
+//#define PI 3.14159265
+//float main()
+//{
+//	float r, s;
+//	scanf_s("%f", &r);
+//	s = PI * r * r;
+//	printf("s=%f\n", s);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//float main()
+//{
+//    float C, F;
+//	scanf_s("%f",&F);
+//	C = 5.0 / 9.0 * (F - 32);
+//	printf("area is C=%f\n", C);
+//	
+//}
+//#include<stdio.h>
+//float main()
+//{
+//	float a, sum;
+//	a = 1000;
+//	sum = a * 0.0036 + a * 0.0225 + a * 0.0198 + 1000;
+//	printf("sum=%f\n", sum);
+//	return 0;
+//
+//}
+#include<stdio.h>
+float main()
+{
+	float a, b, c;
+	scanf_s("%f %f %f", &a, &b, &c);
+	if (a > b > c)
+		printf("max=%f\n", a);
+	if else(c > a > b)
+		printf("max=%f\n", c);
+	if else(b > a > c)
+		printf("max=%f\n", b);
+	else(c > b > a)
+		printf("max=%f\n", c)
+		return 0;
+	88
+	
+}
